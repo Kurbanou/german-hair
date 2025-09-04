6c0feaadcdbcd3f87e426bddb587c11e2262b45e
Farid Kurbanou <kurbanou.faridun@gmail.com>
2025-09-04 16:08:48 +0300

    review  add

--- inc/carbon-fields.php
+++ inc/carbon-fields.php
@@ -3,27 +3,21 @@
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
-Container::make('post_meta', 'Блок FAQ')
+Container::make('post_meta', 'Контент страницы')
     ->where('post_type', '=', 'page')
     ->add_fields([
-        Field::make('text', 'faq_title', 'Заголовок блока'),
+        // FAQ
+        Field::make('separator', 'faq_separator', 'Блок FAQ'),
+        Field::make('text', 'faq_title', 'Заголовок FAQ'),
         Field::make('complex', 'faq_items', 'Вопросы и ответы')
             ->add_fields([
                 Field::make('text', 'question', 'Вопрос'),
                 Field::make('rich_text', 'answer', 'Ответ'),
-            ])
-    ]);
+            ]),
 
-
-
-use Carbon_Fields\Container;
-use Carbon_Fields\Field;
-
-// Отзывы, привязанные к конкретной странице
-Container::make('post_meta', 'Блок отзывов')
-    ->where('post_type', '=', 'page')
-    ->add_fields([
-        Field::make('text', 'reviews_title', 'Заголовок блока'),
+        // Отзывы
+        Field::make('separator', 'reviews_separator', 'Блок отзывов'),
+        Field::make('text', 'reviews_title', 'Заголовок отзывов'),
         Field::make('complex', 'page_reviews', 'Отзывы')
             ->add_fields([
                 Field::make('text', 'name', 'Имя'),
