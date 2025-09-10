22475d752129cdb18b2d954e7b3eea57e825a353
Farid Kurbanou <kurbanou.faridun@gmail.com>
2025-09-10 15:42:56 +0300

    contacts add

--- template-parts/contacts/contacts-title.php
+++ template-parts/contacts/contacts-title.php
@@ -6,16 +6,15 @@
 
 ?>
 
-<section class="contacts-title">
+<section class="contacts title">
+  <div class="section-bg ">
+    <svg width="1920" height="189" viewBox="0 0 1920 189" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M-169 1C-169 1 292.393 128.603 601.743 146.153C840.648 159.706 978.505 152.527 1214.27 120.409C1404.53 94.4909 1538.24 83.5 1736.5 83.5C1918.52 83.5 2161.05 148.995 2293 188" stroke="#967866" stroke-opacity="0.2" />
+      <rect x="341" y="107" width="18" height="18" rx="9" fill="#EAE4E0" />
+    </svg>
+  </div>
   <div class="container">
     <h1 class="h1 text-second-dark">Профессиональная студия наращивания волос German Hair</h1>
-
-
-  </div>
-  <div class="section-bg-mobile">
-    <svg width="480" height="39" viewBox="0 0 480 39" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M0 38.1337C0 38.1337 93.7182 8.95032 156.553 4.93666C205.08 1.837 233.081 3.47895 280.971 10.8244C319.616 16.752 343.957 25.7308 383.746 27.8269C420.678 29.7726 453.198 9.62276 480 0.702148" stroke="#967866" stroke-opacity="0.2" />
-      <rect x="51" y="13.6221" width="16" height="16" rx="8" fill="#EAE4E0" />
-    </svg>
+    <p> Свяжитесь с нами по телефону, электронной почте или через социальные сети. </p>
   </div>
 </section>
