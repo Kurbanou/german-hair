43fbf379bb155a610efc11b93eb2c8007a0e96ca
Farid Kurbanou <kurbanou.faridun@gmail.com>
2025-09-24 16:41:24 +0300

    fix

--- template-parts/about/about-mission.php
+++ template-parts/about/about-mission.php
@@ -7,23 +7,24 @@
 
 
 
-<section class="about-mission">    
+<section class="about-mission">
     <div class="container">
         <div class="about-mission__content_item text">
             <h2 class="h2 text-second-dark ">Наша миссия</h2>
             <p>Сделать наращивание волос максимально безопасным, естественным и комфортным.</p>
             <p>Мы работаем с любыми типами волос – короткими, тонкими, вьющимися, а также предлагаем решения для мужчин и детей.</p>
         </div>
-    </div>    
-    
-    <div class="about-mission__content_item img">
-                <img src="<?php echo get_template_directory_uri() . '/assets/images/about/mission.jpg'; ?>" alt="img">
+        <div class="about-mission__content_item img">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/about/mission.jpg'; ?>" alt="img">
+        </div>
     </div>
-    
+
+
+
     <div class="section-bg-mobile">
         <svg width="480" height="39" viewBox="0 0 480 39" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 38.4315C0 38.4315 93.7182 9.24817 156.553 5.23451C205.08 2.13485 233.081 3.7768 280.971 11.1223C319.616 17.0498 343.957 26.0286 383.746 28.1248C420.678 30.0705 453.198 9.92061 480 1" stroke="#967866" stroke-opacity="0.2"/>
-            <rect x="50" y="14" width="16" height="16" rx="8" fill="#EAE4E0"/>
+            <path d="M0 38.4315C0 38.4315 93.7182 9.24817 156.553 5.23451C205.08 2.13485 233.081 3.7768 280.971 11.1223C319.616 17.0498 343.957 26.0286 383.746 28.1248C420.678 30.0705 453.198 9.92061 480 1" stroke="#967866" stroke-opacity="0.2" />
+            <rect x="50" y="14" width="16" height="16" rx="8" fill="#EAE4E0" />
         </svg>
-    </div>     
+    </div>
 </section>
