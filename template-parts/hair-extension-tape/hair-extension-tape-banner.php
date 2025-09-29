b36eaf2af0389b1abd9f2a5a3591b68b36421288
Фарид Курбанов <kf.dev@german-web.ru>
2025-09-29 09:36:24 +0300

    mod tape

--- template-parts/hair-extension-tape/hair-extension-tape-banner.php
+++ template-parts/hair-extension-tape/hair-extension-tape-banner.php
@@ -27,13 +27,25 @@ $fon = get_field('fon');
     <?php endif; ?>
     <div class="container">
       <div class="tape-banner__content">
-        <h1 class="h1 text-shadow">Ленточное наращивание волос в салоне</h1><?php get_logo(); ?>
+
+        <div class="banner_title">
+          <h1 class="text-shadow">Ленточное наращивание волос в салоне </h1>
+          <?php get_logo(); ?>
+        </div>
+
         <h2>Объем и естественность за 30 минут</h2>
         <p class="banner-p">Хотите густые и длинные волосы без вреда для своих? <br />Ленточное наращивание – это быстрый и безопасный способ добавить объем и длину. <br />Всего за 30 минут вы получите идеальный результат с эффектом натуральных волос.</p>
         <div class="cat">
           <span>Запишитесь сейчас и получите бесплатную консультацию!</span>
         </div>
         <button class="btn btn-light btn-glowing">Записаться</button>
+        <div class="discounts">
+          <div class="discount">
+            <div class="proc">- 50%</div>
+            <div class="text">на первое наращивание</div>
+          </div>
+          <button class="btn btn-light btn-glowing">узнать стоимость со скидкой</button>
+        </div>
       </div>
     </div>
     </section>
