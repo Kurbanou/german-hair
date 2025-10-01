758c596f609d2e106ae95c439d83446730a67e72
Фарид Курбанов <kf.dev@german-web.ru>
2025-10-01 16:06:00 +0300

    fix

--- template-parts/hair-extension-capsule/hair-extension-capsule-banner.php
+++ template-parts/hair-extension-capsule/hair-extension-capsule-banner.php
@@ -35,26 +35,18 @@ $contacts_block = get_contacts_block_data();
     <?php endif; ?>
     <div class="container">
       <div class="capsule-banner__content">
-
         <div class="banner_title">
           <h1 class="text-shadow">Капсульное наращивание волос в салоне</h1>
           <?php get_logo(); ?>
         </div>
-
         <h2>Ваши роскошные локоны за один визит</h2>
         <p class="banner-p">Мечтаете о длинных, густых волосах, которые выглядят на 100% естественно? <br />Капсульное наращивание в нашем салоне — это современное решение для мгновенного преображения. Всего за 2-3 часа вы получите идеальные волосы желаемой длины и объема, которые невозможно отличить от натуральных.</p>
-        <!-- <div class="cat">
-          <span>Запишитесь сейчас и получите бесплатную консультацию!</span>
-        </div> -->
-
         <div class="discounts">
           <button class="btn btn-light btn-glowing">Записаться</button>
           <div class="discount">
-            <!-- <div class="proc">- 10%</div> -->
             <div class="text">Горячее капсульное наращивание волос вы можете сделать <br />в нашем салоне <?php echo esc_html(wp_strip_all_tags($contacts_block[0]['content'][0]['value'])); ?>
             </div>
           </div>
-          <!-- <?php get_icon('bant', 'discounts-icon') ?> -->
         </div>
       </div>
     </div>
